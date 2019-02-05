f2135748dc9320048bdbef4c98a28cd7836bee5b
Max Ovsiankin <max@ovsankin.com>
2019-02-04 22:48:28 -0800

    Basic website

--- update_bootstrap.sh
+++ update_bootstrap.sh
@@ -3,7 +3,8 @@
 # A useful script to download the latest version of bootstrap and jquery
 
 rm -rf node_modules package-lock.json
-npm install bootstrap@4 jquery@3
+npm install popper.js@^1.14.6 jquery@3
+npm install bootstrap@4 
 
 rm -rf _sass/bootstrap
 mkdir -p _sass/bootstrap
